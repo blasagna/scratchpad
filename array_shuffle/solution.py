0d5cbb9a667cc0289d7766b81857cd990fc481a5
Bob DiMaiolo <bdimaiolo@gmail.com>
2026-06-12 16:49:38 -0400

    array shuffle with slicing plus tests

--- array_shuffle/solution.py
+++ array_shuffle/solution.py
@@ -1,21 +1,8 @@
-import itertools
-
-"""
-def shuffle(, nums: list[int], n: int) -> List[int]:
-    x = nums[:n]
-    y = nums[n:]
-    merged = []
-    for i in range(n):
-        merged.append(x[i])
-        merged.append(y[i])
-    return merged
-"""
-
 def shuffle(nums: list[int], n: int) -> list[int]:
-    x = nums[:n]
-    y = nums[n:]
-    return list(itertools.chain.from_iterable(zip(x, y)))
+    res = [0] * len(nums)
+    res[::2] = nums[:n]
+    res[1::2] = nums[n:]
+    return res
 
 if __name__ == "__main__":
     print(shuffle([1, 3, 5, 2, 4, 6], 3))
-
