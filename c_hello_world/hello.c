6a2da0fde50677ba598731082792884f3ff4484e
Bob DiMaiolo <bdimaiolo@gmail.com>
2026-06-22 21:25:36 -0400

    little book of c hello world with bazel build

---
+++ c_hello_world/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// int main(void) {
+int main() {
+    printf("Hello, world!\n");
+    printf("Hello, blasagna!\n");
+    
+    // puts() writes the string s and a trailing newline to stdout.
+    int res_puts = puts("Hello");
+    printf("puts returns a nonnegative number on success, or EOF on error. result: %d\n", res_puts);
+    if (res_puts == EOF) {
+        printf("puts failed\n");
+    }
+//    return 0;
+}
