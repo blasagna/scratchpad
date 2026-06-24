0473c81090be460e468123e87b30519374d81bd9
Bob DiMaiolo <bdimaiolo@gmail.com>
2026-06-24 15:49:57 -0400

    c math recursion library

--- c_little_book/recursion/main.c
+++ c_little_book/recursion/main.c
@@ -1,43 +1,6 @@
 #include <stdio.h>
 
-void count_down(int n) {
-    if (n == 0) {
-        return;
-    }
-    printf("%d ", n);
-    count_down(n - 1);
-}
-
-void count_up(int current, int n) {
-   if (current > n) {
-       return;
-   }
-
-   printf("%d ", current);
-
-   count_up(current + 1, n);
-}
-
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-
-int sum_to_n(int n) {
-    if (n == 1) {
-        return 1;
-    }
-    return n + sum_to_n(n - 1);
-}
-
-int power(int a, int b) {
-    if (b == 1) {
-        return a;
-    }
-    return a * power(a, b - 1);
-}
+#include "math.h"
 
 int main(void) {
     int count_down_start = 10;
---
+++ c_little_book/recursion/math.h
@@ -0,0 +1,14 @@
+#ifndef RECURSION_MATH_H
+#define RECURSION_MATH_H
+
+void count_down(int n);
+
+void count_up(int current, int n);
+
+int factorial(int n);
+
+int sum_to_n(int n);
+
+int power(int a, int b);
+
+#endif
