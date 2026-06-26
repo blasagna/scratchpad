f03c66c76faedb58dd301cda2b6a2835bc1010af
Bob DiMaiolo <bdimaiolo@gmail.com>
2026-06-26 09:46:04 -0400

    arrays and strings examples

---
+++ c_little_book/arrays_strings/example.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+
+#include "example.h"
+
+int sum_array(int arr[], size_t len) {
+    int sum = 0;
+    for (size_t i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+size_t func_sizeof_array(int arr[]) {
+    // demonstrate that sizeof is unexpected in a function scope when array is a parameter
+    return sizeof(arr) / sizeof(arr[0]);
+}
---
+++ c_little_book/arrays_strings/example.h
@@ -0,0 +1,10 @@
+#ifndef ARRAYS_STRINGS_EXAMPLE_H
+#define ARRAYS_STRINGS_EXAMPLE_H
+
+#include <stddef.h>
+
+int sum_array(int arr[], size_t len);
+
+size_t func_sizeof_array(int arr[]);
+
+#endif
